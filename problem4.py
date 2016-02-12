e440e631809c7afc4cc5c693abf88905c45a1eb3
Travis Bumgarner <Travis.Bumgarner@gmail.com>
2016-02-12 14:24:44 -0500

    Solved Problem 4

--- problem4.py
+++ problem4.py
@@ -1,19 +1,16 @@
 def isPal(num):
+    #print(num)
     num = str(num)
     if(len(num) == 0) or (len(num) ==1 ):
-        print(num)
         return True
-        
     elif len(num) >= 2:
-        print("length >=2")
-        if num[0] == num [-0]:
+        if num[0] == num [-1]:
             return isPal(num[1:-1])
         else:
             return False
 largestVal = 0
 val = 0
 
-'''
 for i in range(999,0,-1):
     for j in range(999,0,-1):
         val = i*j
@@ -22,5 +19,3 @@ for i in range(999,0,-1):
                 largestVal = val
                 print(val)
 print(largestVal)
-'''
-"900099"
